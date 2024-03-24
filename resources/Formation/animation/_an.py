f7edfcee2f6abd40b7bf230c1d0c28d96593d6d6
letonn <164564472+letonn@users.noreply.github.com>
2024-03-24 08:31:06 +0300

    update _an.py

--- resources/Formation/animation/_an.py
+++ resources/Formation/animation/_an.py
@@ -1,5 +1,5 @@
-#   ZEDThon - USERBOT
-#   TELE - @ZedThon
+#   LITHON - USERBOT
+#   TELE - @A1DIIU
 
 #  ======================================================= #
 
@@ -7,7 +7,7 @@ from userbot.Config import Config
 
 USERID = Config.OWNER_ID 
 ALIVE_NAME = Config.ALIVE_NAME
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Zed Userbot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LIT Userbot"
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 
 #  ======================================================= #
